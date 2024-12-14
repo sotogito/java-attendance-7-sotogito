0d70e262ea3a7d7ba2f1b7b87fdc717bf62729c8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-14 17:38:07 +0900

    feat : input 작성

---
+++ src/main/java/attendance/view/Input.java
@@ -0,0 +1,42 @@
+package attendance.view;
+
+import attendance.constants.Function;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static String inputFunction(){
+        System.out.println("오늘은 12월 13일 금요일입니다. 기능을 선택해 주세요.");
+        for(Function function : Function.values()){
+            System.out.println(function);
+        }
+        return Console.readLine();
+    }
+
+    public static String inputStudentName(){
+        System.out.println("닉네임을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+
+    public static String inputToEditStudentName(){
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+
+    public static int inputChangeDay(){
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+
+        try {
+            return Integer.parseInt(Console.readLine());
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("잘못된 형식을 입력하였습니다.");
+        }
+
+    }
+
+    public static String inputTime(){
+        System.out.println("언제로 변경하겠습니까?");
+        return Console.readLine();
+    }
+}
