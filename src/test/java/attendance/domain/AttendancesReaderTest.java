feba88e6958488fb8c39b17a1d78bdac1fd3cf9f
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-14 13:59:38 +0900

    test : 입출력 파일 기본 읽어오기 확인

---
+++ src/test/java/attendance/domain/AttendancesReaderTest.java
@@ -0,0 +1,15 @@
+package attendance.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AttendancesReaderTest {
+
+    @Test
+    void 출력확인(){
+        AttendancesReader reader = new AttendancesReader();
+        reader.read();
+    }
+
+}
